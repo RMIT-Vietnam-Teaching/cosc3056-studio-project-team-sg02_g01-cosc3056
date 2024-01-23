fdcc1abb060a4c22fda123cf2bf23a03158fb275
Hung <s4061864@rmit.edu.vn>
2024-01-23 20:27:41 +0700

    Level 2 subtask a changes

---
+++ Test_LVL1/JavaClass/Persona.java
@@ -0,0 +1,25 @@
+public class Persona {
+    public String name;
+    public int age;
+    public String location;
+    public String background;
+    public String quotes;
+    public String needs;
+    public String goals;
+    public String skills;
+    public String img_path;
+
+    public Persona(){
+    }
+    public Persona(String name, int age, String location, String background, String quotes, String needs, String goals, String skills, String img_path){
+        this.name = name;
+        this.age = age;
+        this.location = location;
+        this.background = background;
+        this.quotes = quotes;
+        this.needs = needs;
+        this.goals = goals;
+        this.skills = skills;
+        this.img_path = img_path;
+    }
+}
