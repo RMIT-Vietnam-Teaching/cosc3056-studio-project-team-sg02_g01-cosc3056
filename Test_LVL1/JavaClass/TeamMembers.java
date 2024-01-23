c252018eb5553baf5e6db903e2af1c5fe81ae2f0
Hung <s4061864@rmit.edu.vn>
2024-01-23 20:39:34 +0700

    add Team Member Class

---
+++ Test_LVL1/JavaClass/TeamMembers.java
@@ -0,0 +1,16 @@
+public class TeamMember {
+    public String id;
+    public String name;
+    public String img_path;
+    
+    
+    public TeamMember(){
+    }
+
+    public TeamMember(String id, String name, String img_path) {
+        this.id = id;
+        this.name = name;
+        this.img_path = img_path;
+    }
+
+}
