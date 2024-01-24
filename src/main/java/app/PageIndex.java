4ab1dcd672ef31921bc7db2a484a7d5fbff5eece
Quyen <s3983370@rmit.edu.vn>
2024-01-24 22:32:04 +0700

    Q

--- src/main/java/app/PageIndex.java
+++ src/main/java/app/PageIndex.java
@@ -30,71 +30,86 @@ public class PageIndex implements Handler {
         // Create a simple HTML webpage in a String
         String html = "<html>";
 
-        // Add some Header information
+        // Header information
         html = html + "<head>" + 
                "<title>Homepage</title>";
 
-        // Add some CSS (external file)
+        // CSS (external file)
         html = html + "<link rel='stylesheet' type='text/css' href='common.css' />";
+        html = html + "<link rel='stylesheet' type='text/css' href='PageIndex.css' />";
         html = html + "</head>";
 
-        // Add the body
+        // Open the body
         html = html + "<body>";
 
-        // Add the topnav
-        // This uses a Java v15+ Text Block
+        // The topnav/header
         html = html + """
-            <div class='topnav'>
-                <a href='/'>Homepage</a>
-                <a href='mission.html'>Our Mission</a>
-                <a href='page2A.html'>Sub Task 2.A</a>
-                <a href='page2B.html'>Sub Task 2.B</a>
-                <a href='page3A.html'>Sub Task 3.A</a>
-                <a href='page3B.html'>Sub Task 3.B</a>
-            </div>
+            <header>
+                <div class="nav">
+                    <a class="logo" href="/"><img src="logo-web.jpg" alt="logo" /></a>
+                    <p>Website name</p>
+                    <a class="aboutUs visited" href="mission.html">Our Mission</a>
+                    <a class="visited" href="options.html#Basic">Basic options</a>
+                    <a class="visited" href="options.html#Advanced">Advanced options</a>
+                    <a class="getStarted" href="options.html">Get started</a>
+                </div>
+            </header>
         """;
 
-        // Add header content block
+        // Open main
+        html = html + "<main>";
+
+        // Temperature
         html = html + """
-            <div class='header'>
-                <h1>
-                    <img src='logo.png' class='top-image' alt='RMIT logo' height='75'>
-                    Homepage
-                </h1>
+            <div class="temperature">
+                <h1>GLOBAL AVERAGE TEMPERATURE</h1>
+                <div class="info">
+                    <div class="rangeYr">
+                        <p>In 265 years</p>
+                        <h2>
+        """;
+        // Add avg temp
+
+        html = html + """
+                        °C</h2>
+                    </div>
+                    <img src="icon-increase.jpg" alt="increase" />
+                    <div class="years">
+                        <p>1750</p>
+                        <h3>
+        """;
+        // Add temp in year 1750
+
+        html = html + """
+                        °C</h3>
+                        <p>2015</p>
+                        <h3>
+        """;
+        // Add temp in year 2015
+
+        html = html + """
+                        °C</h3>
+                    </div>
+                    <img src="icon-temp.jpg" alt="temperature" />
+                </div>
             </div>
         """;
-
-        // Add Div for page Content
-        html = html + "<div class='content'>";
-
-        // Add HTML for the page content
-        html = html + """
-            <p>Homepage content</p>
-            """;
-
-        // Get the ArrayList of Strings of all LGAs
-        ArrayList<String> lgaNames = getLGAs2016();
-
-        // Add HTML for the LGA list
-        html = html + "<h1>All 2016 LGAs in the Voice to Parliament database</h1>" + "<ul>";
-
-        // Finally we can print out all of the LGAs
-        for (String name : lgaNames) {
-            html = html + "<li>" + name + "</li>";
-        }
-
-        // Finish the List HTML
-        html = html + "</ul>";
-
-        // Close Content div
-        html = html + "</div>";
+        
+        // Close main
+        html = html + "</main>";
 
         // Footer
         html = html + """
-            <div class='footer'>
-                <p>COSC3056 - Studio Project Starter Code (Sep23)</p>
-            </div>
-        """;
+            <footer>
+                <a href=""><img src="/" alt="logo" /></a>
+                <div class="link">
+                    <a href="">Link</a>
+                    <a href="">Link</a>
+                    <a href="">Link</a>
+                    <a href="">Link</a>
+                </div>
+            </footer>
+         """;
 
         // Finish the HTML webpage
         html = html + "</body>" + "</html>";
@@ -103,59 +118,5 @@ public class PageIndex implements Handler {
         // DO NOT MODIFY THIS
         // Makes Javalin render the webpage
         context.html(html);
-    }
-
-
-    /**
-     * Get the names of the LGAs in the database.
-     */
-    public ArrayList<String> getLGAs2016() {
-        // Create the ArrayList of LGA objects to return
-        ArrayList<String> lgas = new ArrayList<String>();
-
-        // Setup the variable for the JDBC connection
-        Connection connection = null;
-
-        try {
-            // Connect to JDBC data base
-            connection = DriverManager.getConnection(JDBCConnection.DATABASE);
-
-            // Prepare a new SQL Query & Set a timeout
-            Statement statement = connection.createStatement();
-            statement.setQueryTimeout(30);
-
-            // The Query
-            String query = "SELECT * FROM LGA WHERE year='2016'";
-            
-            // Get Result
-            ResultSet results = statement.executeQuery(query);
-
-            // Process all of the results
-            while (results.next()) {
-                String name16  = results.getString("name");
-
-                // Add the lga object to the array
-                lgas.add(name16);
-            }
-
-            // Close the statement because we are done with it
-            statement.close();
-        } catch (SQLException e) {
-            // If there is an error, lets just pring the error
-            System.err.println(e.getMessage());
-        } finally {
-            // Safety code to cleanup
-            try {
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (SQLException e) {
-                // connection close failed.
-                System.err.println(e.getMessage());
-            }
-        }
-
-        // Finally we return all of the lga
-        return lgas;
     }
 }
