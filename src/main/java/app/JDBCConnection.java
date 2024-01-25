71b152abb47153ce8b946e227dc9940f5feaff64
DangHynh <s4045607@rmit.edu.vn>
2024-01-26 00:42:45 +0700

    delete lga

--- src/main/java/app/JDBCConnection.java
+++ src/main/java/app/JDBCConnection.java
@@ -36,60 +36,8 @@ public class JDBCConnection {
      * @return
      *    Returns an ArrayList of LGA objects
      */
-    public ArrayList<LGA> getLGAs2016() {
-        // Create the ArrayList of LGA objects to return
-        ArrayList<LGA> lgas = new ArrayList<LGA>();
-
-        // Setup the variable for the JDBC connection
-        Connection connection = null;
-
-        try {
-            // Connect to JDBC data base
-            connection = DriverManager.getConnection(DATABASE);
-
-            // Prepare a new SQL Query & Set a timeout
-            Statement statement = connection.createStatement();
-            statement.setQueryTimeout(30);
-
-            // The Query
-            String query = "SELECT * FROM LGA WHERE year='2016'";
-            
-            // Get Result
-            ResultSet results = statement.executeQuery(query);
-
-            // Process all of the results
-            while (results.next()) {
-                // Lookup the columns we need
-                int code     = results.getInt("code");
-                String name  = results.getString("name");
-
-                // Create a LGA Object
-                LGA lga = new LGA(code, name, 2016);
-
-                // Add the lga object to the array
-                lgas.add(lga);
-            }
-
-            // Close the statement because we are done with it
-            statement.close();
-        } catch (SQLException e) {
-            // If there is an error, lets just pring the error
-            System.err.println(e.getMessage());
-        } finally {
-            // Safety code to cleanup
-            try {
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (SQLException e) {
-                // connection close failed.
-                System.err.println(e.getMessage());
-            }
-        }
 
         // Finally we return all of the lga
-        return lgas;
-    }
 
     // TODO: Add your required methods here
 
