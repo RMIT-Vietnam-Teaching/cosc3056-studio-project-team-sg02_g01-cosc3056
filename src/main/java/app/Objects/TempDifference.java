61046b625d9d93f28383190837da319ad642cfd0
DangHynh <s4045607@rmit.edu.vn>
2024-01-25 21:33:26 +0700

    add TempDifference object

---
+++ src/main/java/app/Objects/TempDifference.java
@@ -0,0 +1,20 @@
+package app.Objects;
+
+public class TempDifference {
+    int ID;
+    String name;
+    double difference;
+    public TempDifference(){
+
+    }
+    public void setID(int ID){
+        this.ID = ID;
+    }
+    public void setname(String name){
+        this.name = name;
+    }
+    public void setDifference(double firstYear, double lastYear){
+        this.difference = lastYear = firstYear;
+    }
+
+}
