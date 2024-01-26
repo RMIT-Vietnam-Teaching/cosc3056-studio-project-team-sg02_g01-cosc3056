2e5b29a4025cb857fcd66c40de4343131ce708d6
DangHynh <s4045607@rmit.edu.vn>
2024-01-26 14:24:15 +0700

    page 2b java

--- src/main/java/app/Objects/TempDifference.java
+++ src/main/java/app/Objects/TempDifference.java
@@ -4,6 +4,10 @@ public class TempDifference {
     int ID;
     String name;
     double difference;
+    double differencePercent;
+    boolean up = false;
+    public double tFirstYear;
+    public double tLastYear;
     public TempDifference(){
 
     }
@@ -14,17 +18,34 @@ public class TempDifference {
         this.name = name;
     }
     public void setDifference(double tFirstYear, double tLastYear){
-        this.difference = Math.abs(Math.abs(tLastYear) - Math.abs(tFirstYear)); //temp
+        this.tLastYear = tLastYear;
+        this.tFirstYear = tFirstYear;
+        
+        if(tLastYear - tFirstYear < 0){
+            up = false;
+        } //temp
+        else{
+            up = true;
+        }
+        this.difference = Math.abs(tLastYear - tFirstYear);
+        this.differencePercent = Math.abs(100-(tLastYear/tFirstYear)*100);
+
     }
 
 
     public double getDifference(){
         return difference;
     }
+    public double getDifferencePercent(){
+        return differencePercent;
+    }
     public int getID(){
         return ID;
     }
     public String getName(){
         return name;
+    }
+    public boolean getUp(){
+        return up;
     }
 }
