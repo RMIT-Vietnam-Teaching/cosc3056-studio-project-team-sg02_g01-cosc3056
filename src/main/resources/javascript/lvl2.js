537f7d935d04ad9cf157354abcf3af29ed49c429
terrapin04 <61127023+terrapin04@users.noreply.github.com>
2024-01-27 12:06:24 +0700

    lvl2.js changes - year start + end dropdown menus

--- src/main/resources/javascript/lvl2.js
+++ src/main/resources/javascript/lvl2.js
@@ -17,4 +17,40 @@ document.addEventListener('DOMContentLoaded', function(){
             }
         }
     });
+
+    //Year Start - End dropdown menus
+    var yearStartSelect = document.querySelector('#year_start'); //Year start dropdown menu
+    var yearEndSelect = document.querySelector('#year_end'); //Year end dropdown menu
+    var yearEndOptions = document.querySelectorAll('#year_end option'); //Year end dropdown options
+    var submitBtt = document.querySelector("button[type = 'submit']");
+
+    //Listens for when the year_start dropdown is selected
+    yearStartSelect.addEventListener("change", function() {
+        //Selected option of year_start
+        var yearStartSelected = yearStartSelect.options[yearStartSelect.selectedIndex].value;
+        console.log("Selected year: " + yearStartSelected); //Checking on console if action is detected
+
+        //Hide options that are smaller than selected year_start
+        yearEndOptions.forEach(function(option) {
+            //console.log(option.value);
+
+            if (parseInt(option.value) < parseInt(yearStartSelected)) {
+                option.setAttribute("hidden", '');
+                console.log("Hidden: " + option.value);
+            }
+            else {
+                option.removeAttribute("hidden");
+                console.log("Not hidden: " + option.value);
+            }
+        });
+
+        //Enable year_end dropdown
+        yearEndSelect.disabled = false;
+    });
+
+    //Enable submit button when year range is selected
+    yearEndSelect.addEventListener("change", function(){
+        submitBtt.disabled = false;
+        submitBtt.innerHTML = "Submit";
+    });
 })
