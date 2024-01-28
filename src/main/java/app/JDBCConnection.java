b11ca6b67c7411a14fd481ce7202c47675efc5c0
terrapin04 <61127023+terrapin04@users.noreply.github.com>
2024-01-28 19:54:30 +0700

    Add getYearWorld() to get available years from worldTemp table for PageST2A_world

--- src/main/java/app/JDBCConnection.java
+++ src/main/java/app/JDBCConnection.java
@@ -448,6 +448,56 @@ public class JDBCConnection {
         return AllYears;
     }
 
+    public ArrayList<Integer> getYearWorld() {
+        // Create the ArrayList of LGA objects to return
+        ArrayList<Integer> AllYears = new ArrayList<Integer>();
+ 
+        // Setup the variable for the JDBC connection
+        Connection connection = null;
+ 
+        try {
+            // Connect to JDBC data base
+            connection = DriverManager.getConnection(DATABASE);
+ 
+            // Prepare a new SQL Query & Set a timeout
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30);
+ 
+            // The Query
+            String query = "SELECT DISTINCT year FROM worldTemp;";
+           
+            // Get Result
+            ResultSet results = statement.executeQuery(query);
+ 
+            // Process all of the results
+            while (results.next()) {
+                // Lookup the columns we need
+                int Year = results.getInt("year");
+ 
+               
+                AllYears.add(Year);
+            }
+ 
+            // Close the statement because we are done with it
+            statement.close();
+        } catch (SQLException e) {
+            // If there is an error, lets just pring the error
+            System.err.println(e.getMessage());
+        } finally {
+            // Safety code to cleanup
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                // connection close failed.
+                System.err.println(e.getMessage());
+            }
+        }
+ 
+       
+        return AllYears;
+    }
 
 
 
