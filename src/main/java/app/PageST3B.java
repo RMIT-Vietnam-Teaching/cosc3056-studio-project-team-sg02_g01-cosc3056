330b666ebed27b946a57a9a079af368870d94fc2
terrapin04 <61127023+terrapin04@users.noreply.github.com>
2024-01-31 02:58:00 +0700

    Change javascript in lvl3B java html part to linking to the new .js file

--- src/main/java/app/PageST3B.java
+++ src/main/java/app/PageST3B.java
@@ -44,75 +44,7 @@ public class PageST3B implements Handler {
 
         // Add JavaScript to display Cities / States of selected country
         html += """
-          <script>
-            document.addEventListener('DOMContentLoaded', function(){
-              var radioButtonState = document.querySelector('#state');
-              var radioButtonCity  = document.querySelector('#city');
-              
-              //Check every time a radio button selection is changed
-              var radioButtons = document.querySelectorAll('.region_radio');
-              radioButtons.forEach(function(radio) {
-                radio.onchange = function() {
-                  //console.log(radio.value);
-                  var selectCountryCode = document.querySelector('#countrySubmit').value;
-                  console.log(selectCountryCode);
-                  if (radioButtonState.checked) {
-                    //console.log("State");
-                    document.querySelectorAll('#stateSubmit option').forEach(function(option) {
-                      if (option.className != selectCountryCode) {
-                        option.setAttribute("hidden", '');
-                      }
-                      else {
-                        option.removeAttribute("hidden");
-                      }
-                    });
-                  }
-                  else if (radioButtonCity.checked) {
-                    //console.log("City");
-                    document.querySelectorAll('#citySubmit option').forEach(function(option) {
-                      if (option.className != selectCountryCode) {
-                        option.setAttribute("hidden", '');
-                      }
-                      else {
-                        option.removeAttribute("hidden");
-                      }
-                    });
-                  }
-                }
-              })
-
-              //Check every time a new country is selected
-              var selectCountry = document.querySelector('#countrySubmit')
-              selectCountry.onchange = function() {
-                var selectCountryCode = document.querySelector('#countrySubmit').value;
-                  console.log(selectCountryCode);
-                  if (radioButtonState.checked) {
-                    //console.log("State");
-                    document.querySelectorAll('#stateSubmit option').forEach(function(option) {
-                      if (option.className != selectCountryCode) {
-                        option.setAttribute("hidden", '');
-                      }
-                      else {
-                        option.removeAttribute("hidden");
-                      }
-                    });
-                  }
-                  else if (radioButtonCity.checked) {
-                    //console.log("City");
-                    document.querySelectorAll('#citySubmit option').forEach(function(option) {
-                      if (option.className != selectCountryCode) {
-                        option.setAttribute("hidden", '');
-                      }
-                      else {
-                        option.removeAttribute("hidden");
-                      }
-                    });
-                  }
-              }
-              
-            })
-
-          </script>
+          <script src='lvl3B_relevantCityState.js'></script>
             """;
 
         // Close head
