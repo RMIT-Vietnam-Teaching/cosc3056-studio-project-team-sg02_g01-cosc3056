c175d7eed7ffabe6562c08a91594902808bf5c2f
DangHynh <s4045607@rmit.edu.vn>
2024-01-30 23:30:37 +0700

    change layout 2b

--- src/main/java/app/Page2B/PageST2B_cities.java
+++ src/main/java/app/Page2B/PageST2B_cities.java
@@ -96,7 +96,7 @@ public class PageST2B_cities implements Handler {
                     </a>
                     <a href="page2A_country.html">
                         <div class="box">
-                            <img src="country-logo.png" alt="countries">
+                            <img src="country-logo2.png" alt="countries">
                             <h5>Countries</h5>
                         </div>
                     </a>
